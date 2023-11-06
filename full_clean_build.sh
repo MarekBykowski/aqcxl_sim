a8387e97f3fb15249812008580170a2d5705c9b1
Marek Bykowski <marekx.bykowski@intel.com>
2023-11-05 23:55:47 -0800

    Update full_clean_build.sh
    Run either git clean on the repo or run both git clean and rebuilding
    for the perf facility.
    
    Signed-off-by: Marek Bykowski <marekx.bykowski@intel.com>

--- full_clean_build.sh
+++ full_clean_build.sh
@@ -1,8 +1,14 @@
 #!/bin/bash -x
 
-git clean -fdx
 
-cd aqemu/lib/measure
-make clean
-make
-cp libmeasure64.* ../
+if [[ $1 =~ git ]]; then
+    git clean -fdx
+elif [[ $1 =~ build ]]; then
+    git clean -fdx
+    cd aqemu/lib/measure
+    make clean
+    make
+    cp libmeasure64.* ../
+else
+    echo "Run $0 git or build"
+fi
